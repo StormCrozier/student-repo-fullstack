78f1edbb285aa380954497b81ee1b3f46d3c737a
StormCrozier <stormcrozier950@gmail.com>
2022-10-11 15:40:47 -0700

    Added functionality to all files

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,24 @@
-/** Exercise 02 - Reverse **/
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/** Exercise 02 - Reverse * */
 
 // Add your code here
+
+// Function to reverse input
+const reverse = (input) => {
+  let toReturn = '';
+  let temp = [];
+  temp = input.toString();
+  temp = temp.split('');
+  if (temp.length >= 9 || temp.length <= 7) {
+    toReturn = 'Error: Please enter an 8-digit number';
+  } else {
+    temp = temp.reverse();
+    temp = temp.join('');
+    toReturn = `${input} --> ${temp}`;
+  }
+  const currentDiv = document.getElementById('here');
+  currentDiv.textContent = toReturn;
+  currentDiv.style.color = 'red';
+  currentDiv.style.marginTop = '15px';
+};
